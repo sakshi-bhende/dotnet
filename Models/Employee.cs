b3e60997b2623bc6be747039ce7bd1c944bfa688
Sakshi Bhende <103889378+sakshi-bhende@users.noreply.github.com>
2025-01-14 01:03:15 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,94 @@
+﻿using System.Data;
+using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
+namespace MVCDemo.Models
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public int DeptNo { get; set; }
+
+        public static List<Employee> getAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ActsJan25;Integrated Security=True";
+            try
+            {
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Select * from Employee";
+
+                SqlDataReader dr = cmd.ExecuteReader();
+              
+
+                while (dr.Read())
+                {
+                    Employee obj = new Employee();
+                    
+
+                    obj.Name = dr.GetString("Name"); ;
+                    obj.EmpNo = dr.GetInt32("EmpNo");
+                    obj.Basic = dr.GetDecimal("Basic");
+                    obj.DeptNo = dr.GetInt32("DeptNo");
+                    employees.Add(obj);
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return employees;
+        }
+        public static Employee GetSingleEmployee(int EmpNo)
+        {
+            Employee obj = null;
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ActsJan25;Integrated Security=True";
+            try
+            {
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Select * from Employee where EmpNo=@EmpNo";
+                cmd.Parameters.AddWithValue("@EmpNo", EmpNo);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    obj = new Employee();
+                    obj.Name = dr.GetString("Name"); ;
+                    obj.EmpNo = dr.GetInt32("EmpNo");
+                    obj.Basic = dr.GetDecimal("Basic");
+                    obj.DeptNo = dr.GetInt32("DeptNo");
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return obj;
+        }
+
+        
+        }
+    
+}
---
+++ Models/Users.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MVCDemo.Models
+{
+    public class Users
+    {
+
+        public int UserId { get; set; }
+        public string Name { get; set; }
+
+        public int DeptNo { get; set; }
+
+
+        public static List<Users> GetAllUser()
+        {
+            List<Users> user = new List<Users>();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ActsJan25;Integrated Security=True";
+            try
+            {
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Select * from Users";
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+
+                while (dr.Read())
+                {
+                    Users obj = new Users();
+
+
+                    obj.Name = dr.GetString("Name"); ;
+                    obj.UserId = dr.GetInt32("UserId");
+                    // obj.Basic = dr.GetDecimal("Basic");
+                    obj.DeptNo = dr.GetInt32("DeptNo");
+                    user.Add(obj);
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return user;
+        }
+
+        public static void Update(Users obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ActsJan25;Integrated Security=True";
+            try
+            {
+                cn.Open();
+
+                // SqlCommand cmd = cn.CreateCommand();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Update Users set Name=@Name,DeptNo=@DeptNo where UserId=@UserId";
+
+                cmd.Parameters.AddWithValue("@Name", obj.Name);
+                // cmd.Parameters.AddWithValue("@Basic", obj.Basic);
+                cmd.Parameters.AddWithValue("@DeptNo", obj.DeptNo);
+                cmd.Parameters.AddWithValue("@UserId", obj.UserId);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public static Users GetSingleUser(int UserId)
+        {
+            Users obj = null;
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ActsJan25;Integrated Security=True";
+            try
+            {
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Select * from Users where UserID=@UserId";
+                cmd.Parameters.AddWithValue("@UserId", UserId);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    obj = new Users();
+                    obj.Name = dr.GetString("Name"); ;
+                    obj.UserId = dr.GetInt32("UserId");
+                   // obj.Basic = dr.GetDecimal("Basic");
+                    obj.DeptNo = dr.GetInt32("DeptNo");
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return obj;
+        }
+
+
+    }
+}
